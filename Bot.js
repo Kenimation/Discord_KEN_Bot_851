0589fb933ea14650644c94386de1e23840bf827f
Kenimation <46868437+Kenimation@users.noreply.github.com>
2019-02-17 17:08:11 +0800

    Add files via upload

--- Bot.js
+++ Bot.js
@@ -1,195 +1,172 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
 const bot = new Discord.Client();
+const fs = require('fs');
+const xp = require("./xp.json");
+
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err);
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if(jsfile.length <= 0){
+      console.log("Couldn't find commands.");
+      return;
+    }
+  
+    jsfile.forEach((f, i) =>{
+      let props = require(`./commands/${f}`);
+      console.log(`${f} loaded!`);
+      bot.commands.set(props.commands, props);
+    });
+  });
 
 bot.on('ready', () => {
   console.log(`Connect as ${bot.user.tag}!`)
 
-  bot.channels.find(x => x.name === 'general').send('@KEN I am connecting')
+  bot.channels.find(x => x.name === 'bot-testing').send('KEN-Bot is connecting')
+});
+
+bot.on('guildMemberAdd', member => {
+  let memberavatar = member.user.avatarURL
+      let inembed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`Welcome to the Kenimation server.`)
+      .setThumbnail(memberavatar)
+      .setDescription("KEN-Bot 1.0 Helps \n</> for ProcessCommand.")
+      .addField('Name:', `${member}`)
+      .addField('User ID:', "**[" + `${member.id}` + "]**")
+      .addField('Your are the member', `${member.guild.memberCount}`)
+      .addField("Name", `<@` + `${member.id}` + `>`, true)
+      .addField('Server', `${member.guild.name}`, true )
+      .addField("🛠Useful Commands", "`/Help` For command and stuff. \n`/role` to check roles. \n`/report` + [Name] + [Reason] To report someone. \n`/level` to check your level.")
+      .addField("❓Question Commands", "`/Animation` Ask information of animations. \n`/music` How to listen music in the server. \n`/sharescreen` How to sharescreen in the server.")
+      .addField("🎉Funny Commands", "`/ping` Pong! \n`/miss` or [Name] You can miss someone. \n`/come` Notice people that `KEN` is joined discord. \n`/love` to give a love")
+      .setFooter(`Thanks for reading. **${member.guild.name}**`)
+      .setTimestamp(Date.now())
+
+      member.send(inembed);
+
+      let inreportembed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`${member} just join the server,`)
+      .setThumbnail(memberavatar)
+      .addField('Name:', `${member}`)
+      .addField(`${member} join the Server`, '🎉🎉🎉')
+      .addField('The server now as', `${member.guild.memberCount}` + " members")
+      .setFooter(`Hello. **${member.guild.name}**`)
+      .setTimestamp()
+
+      bot.channels.find(x => x.name === 'join-leave-report').send(inreportembed);
+});
+
+bot.on('guildMemberAdd', member => {
+
+  console.log(`${member}`, "has joined" + `${member.guild.name}`)
+
+});
+
+bot.on('guildMemberRemove', member => {
+  let memberavatar = member.user.avatarURL
+      let outembed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`You just left the server, ${member}`)
+      .setThumbnail(memberavatar)
+      .addField('Name:', `${member}`)
+      .addField('Left the Server', '😥😥😥')
+      .addField('Bye Bye 😥', 'We will all miss you!')
+      .addField('The server now as', `${member.guild.memberCount}` + " members")
+      .setFooter(`Goodbye. **${member.guild.name}**`)
+      .setTimestamp()
+
+      member.send(outembed);
+
+      let outreportembed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`${member} just left the server,`)
+      .setThumbnail(memberavatar)
+      .addField('Name:', `${member}`)
+      .addField(`${member} left the Server`, '😥😥😥')
+      .addField('The server now as', `${member.guild.memberCount}` + " members")
+      .setFooter(`Goodbye. **${member.guild.name}**`)
+      .setTimestamp()
+
+      bot.channels.find(x => x.name === 'join-leave-report').send(outreportembed);
+});
+
+bot.on('guildMemberRemove', member => {
+  console.log(`${member}` + "has left" + `${member.guild.name}` + "Sending leave message now")
+  console.log("Leave Message Sent")
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'hi') {
-    msg.reply('welcome to Kenimation Discord Server.');
-  }
-  else if (msg.content === 'hi ken') {
-    msg.reply('welcome to Kenimation Discord Server.');
-  }
-  else if (msg.content === 'hello') {
-    msg.reply('welcome to Kenimation Discord Server.');
-  }
-  else if (msg.content === 'hello ken') {
-    msg.reply('welcome to Kenimation Discord Server.');
-  }
-  else if (msg.content === 'KEN Bot are you good') {
-    msg.reply('I am not sure about this. My owner is first time coding. He has tried very hard to build me...');
-  }
-  else if (msg.content === 'Is Ken a boy') {
-    msg.reply('Yes, maybe...');
-  }
-  else if (msg.content === 'Is Ken a trap') {
-    msg.reply('Sorry,my owner has not told me that...');
-  }
-  else if (msg.content === 'Is Ken gay') {
-    msg.reply('No one knows in the world...');
-  }
-  else if (msg.content === 'Is Tommy a boy') {
-    msg.reply('No, I am sure');
-  }
-  else if (msg.content === 'Is Tommy a trap') {
-    msg.reply('My owner has told me that he is a trap...');
-  }
-  else if (msg.content === 'Is Tommy gay') {
-    msg.reply('No one knows in the world...');
-  }
-  else if (msg.content === 'Is Heyson a boy') {
-    msg.reply('Go find Hit or Miss?');
-  }
-  else if (msg.content === 'Is Heyson a trap') {
-    msg.reply('No, but he may be gay...');
-  }
-  else if (msg.content === 'Is Heyson gay') {
-    msg.reply('Yes, definitely, absolutely');
-  }
-  else if (msg.content === 'Where is 3%') {
-    msg.reply('No one knows in the world...');
-  }
-  else if (msg.content === 'Who is 3%') {
-    msg.reply('My owner said that he do not know...');
-  }
-  else if (msg.content === 'Is ken bad at animating') {
-    msg.reply('I think he is not well done but he has tried very hard :)');
-  }
-  else if (msg.content === 'Is Temmie the best girl') {
-    msg.reply('Tommy think yes but my owner....');
-  }
-  else if (msg.content === 'Is Mary the best girl') {
-    msg.reply('Yes, definitely, absolutely!');
-  }
-  else if (msg.content === 'Is Iommy bad at animating') {
-    msg.reply('He is good at Cinema 4D and 2D Animation :/');
-  }
-  else if (msg.content === 'KEN Bot music') {
-    msg.reply('You may go Music room click `!help` to use Rythm-Bot');
-  }
-  else if (msg.content === ':l') {
-    msg.reply(':l');
-  }
-  else if (msg.content === ':3') {
-    msg.reply(':3');
-  }
-  else if (msg.content === '=_=') {
-    msg.reply('=_=');
-  }
-  if (msg.author == bot.user) { // Prevent bot from responding to its own messages
-      return
+
+    if (msg.author.bot || msg.channel.type !== 'text') return
+	if (!msg.content.toLowerCase().startsWith(config.prefix)) return
+	const args = msg.content.slice(config.prefix.length).trim().split(/ +/g)
+	const cmd = args.shift().toLowerCase()
+	try{
+        cmdFile = require(`./commands/${cmd}.js`)
+		cmdFile.run(bot, msg, args);
+    }catch(e){}
+
+    let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[msg.author.id]){
+    xp[msg.author.id] = {
+      xp: 0,
+      level: 1
+    };
   }
 
-  if (msg.content.startsWith("/")) {
-      processCommand(msg)
+  let curxp = xp[msg.author.id].xp;
+  let curlvl = xp[msg.author.id].level;
+  let nxtLvl = xp[msg.author.id].level * 300;
+  xp[msg.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[msg.author.id].xp){
+    xp[msg.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("GG Level Up!")
+    .setColor("RANDOM")
+    .addField("New Level", curlvl + 1);
+
+    msg.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+
+  let GP = msg.guild.roles.find(`name`, "General Public");
+  if(xp[msg.author.id].level === 5) {
+
+    msg.member.addRole(GP)
+    msg.member.removeRole(AU)
+    msg.member.removeRole(Fans)
+
   }
 
-  if (msg.content.startsWith("?")) {
-      questionCommand(msg)
+  let AU = msg.guild.roles.find(`name`, "Active User");
+  if(xp[msg.author.id].level === 10) {
+    
+    msg.member.addRole(AU)
+    msg.member.removeRole(GP)
+    msg.member.removeRole(Fans)
+
   }
+
+  let Fans = msg.guild.roles.find(`name`, "Fans");
+  if(xp[msg.author.id].level === 20) {
+    
+    msg.member.addRole(Fans)
+    msg.member.removeRole(GP)
+    msg.member.removeRole(AU)
+
+  }
+
+    if (!msg.guild) return;
+    
 });
-
-function processCommand(msg) {
-    let fullCommand = msg.content.substr(1) // Remove the leading exclamation mark
-    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
-
-    console.log("Command received: " + primaryCommand)
-    console.log("Arguments: " + arguments) // There may not be any arguments
-
-    if (primaryCommand == "help") {
-        helpCommand(arguments, msg)
-    } else if (primaryCommand == "multiply") {
-        multiplyCommand(arguments, msg)
-    } else if (primaryCommand == "miss") {
-        missCommand(arguments, msg)
-    } else if (primaryCommand == "skip") {
-        skipCommand(arguments, msg)
-    } else if (primaryCommand == "play") {
-        playCommand(arguments, msg)
-    } else if (primaryCommand == "come") {
-        comeCommand(arguments, msg)
-    } else {
-        msg.channel.send("I don't understand the command. Try `/help` or `Ask Ken`")
-    }
-}
-
-function questionCommand(msg) {
-    let fullCommand = msg.content.substr(1) // Remove the leading exclamation mark
-    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
-
-    console.log("Command received: " + primaryCommand)
-    console.log("Arguments: " + arguments) // There may not be any arguments
-
-    if (primaryCommand == "animation") {
-        animationCommand(arguments, msg)
-    } else if (primaryCommand == "sharescreen") {
-        sharescreenCommand(arguments, msg)
-    } else {
-        msg.channel.send("I don't understand the command. Try `/help` or `Ask Ken`")
-    }
-}
-
-
-
-
-function helpCommand(arguments, msg) {
-    if (arguments.length > 0) {
-        msg.channel.send("It looks like you might need help with " + arguments)
-    } else {
-        msg.channel.send("I'm not sure what you need help with. Try `/help [topic]`")
-    }
-}
-
-function multiplyCommand(arguments, msg) {
-    if (arguments.length < 2) {
-        msg.channel.send("Not enough values to multiply. Try `/multiply 2 4 10` or `/multiply 5.2 7`")
-        return
-    }
-    let product = 1
-    arguments.forEach((value) => {
-        product = product * parseFloat(value)
-    })
-    msg.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
-}
-
-function missCommand(arguments, msg) {
-    if (arguments.length > 0) {
-        msg.channel.send("I bet " + arguments + " doesn't miss ya")
-    } else {
-        msg.channel.send("@everyone Hit or Miss")
-    }
-}
-function comeCommand(arguments, msg) {
-    if (arguments.length > 0) {
-        msg.channel.send(arguments + " ,why not join discord.")
-    } else {
-        msg.channel.send("@everyone KEN has arrived!!!!!! Everyone join discord.")
-    }
-}
-
-function sharescreenCommand(arguments, msg) {
-    if (arguments.length > 0) {
-        msg.channel.send(arguments + "why not watch my sharescreen")
-    } else {
-        msg.channel.send("If you want sharescreen. You have to get sharescreen code right voice channel first.")
-    }
-}
-
-function animationCommand(arguments, msg) {
-    if (arguments.length > 0) {
-        msg.channel.send(arguments + ",can you show me some of animation?")
-    } else {
-        msg.channel.send("If you want animation. You can go animation text room share animations or talk about animation..")
-    }
-}
 
 bot.login(config.token);
